8e9b855b78272e73cf3bdbcea6f178776d5bc428
Marshall Yount <myount@zynga.com>
2012-07-26 14:48:41 -0500

    fix code coverage report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 require 'bundler'
 Bundler.setup
 
-require File.join(File.dirname(__FILE__), '../lib/ball_of_light') # require 'ball_of_light/ball_of_light'
-
 if RUBY_VERSION.to_f == 1.9
   require 'simplecov'
   SimpleCov.start
 end
+
+require 'ball_of_light'
