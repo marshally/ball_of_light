71779ee3bf2744a9bc58c5a5681e6d0a58f38e49
Marshall Yount <myount@zynga.com>
2012-07-26 22:36:41 -0500

    short transitions look better than 5s

--- scripts/0.rb
+++ scripts/0.rb
@@ -27,9 +27,9 @@ controller.instant!(:point => :strobe_fast, :tilt => 80)
 # Have all lights sweep slowly back and forth for 30 sec, cycling through the colors. The lights have a greater range in one axis, so sweep in that direction. Return to center when complete.
 3.times do
   # back 5 seconds
-  controller.transition!(:seconds => 5, :pan => 0, :gobo => 8)
+  controller.transition!(:seconds => 2.5, :pan => 0, :gobo => 8)
   # forward 5 seconds
-  controller.transition!(:seconds => 5, :pan => 255, :gobo => 57)
+  controller.transition!(:seconds => 2.5, :pan => 255, :gobo => 57)
 end
 
 # back to center
@@ -37,16 +37,19 @@ controller.transition!(:seconds => 2, :pan => 0, :tilt => 80)
 
 # Have all lights inscribe a slow spiral, terminating in a circle, as wide as the hardware allows. Curved spirals are hard to program, so you can do an easy square spiral with a couple of do loops. Continue cycling between colors with each loop. (or substitute a cooler routine.)
 # NOTE: this is more of a circle?
-controller.transition!(:seconds => 5, :tilt => 55, :pan => 255)
-controller.transition!(:seconds => 5, :tilt => 30, :pan => 255)
-controller.transition!(:seconds => 5, :tilt => 55, :pan => 255)
-controller.transition!(:seconds => 5, :tilt => 80, :pan => 255)
+controller.transition!(:seconds => 2.5, :tilt => 55, :pan => 255)
+controller.transition!(:seconds => 2.5, :tilt => 30, :pan => 255)
+controller.transition!(:seconds => 2.5, :tilt => 55, :pan => 255)
+controller.transition!(:seconds => 2.5, :tilt => 80, :pan => 255)
 
 # Put all lights in automatic light routine for 30 seconds
 # MCY: not sure what this means?
 
 # Do the spiral in reverse.
-controller.transition!(:seconds => 5, :tilt => 80, :pan => 255)
-controller.transition!(:seconds => 5, :tilt => 55, :pan => 255)
-controller.transition!(:seconds => 5, :tilt => 30, :pan => 255)
-controller.transition!(:seconds => 5, :tilt => 55, :pan => 255)
+controller.transition!(:seconds => 2.5, :tilt => 80, :pan => 255)
+controller.transition!(:seconds => 2.5, :tilt => 55, :pan => 255)
+controller.transition!(:seconds => 2.5, :tilt => 30, :pan => 255)
+controller.transition!(:seconds => 2.5, :tilt => 55, :pan => 255)
+
+controller.center
+controller.instant!(:dimmer => 0)
