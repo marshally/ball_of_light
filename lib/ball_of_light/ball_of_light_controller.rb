8be0c5ab231a5061914f1cd4fa36fef1b1f9fbaa
Marshall Yount <myount@zynga.com>
2012-07-28 11:55:39 -0500

    calibrate additional points

--- lib/ball_of_light/ball_of_light_controller.rb
+++ lib/ball_of_light/ball_of_light_controller.rb
@@ -8,6 +8,26 @@ module BallOfLight
       count.times do |i|
         self << OpenLighting::Devices::ComscanLed.new
       end
+
+      devices.each_with_index {|device, i| device.add_points BallOfLightController.additional_points[i]}
+    end
+
+    def self.points_file
+      ENV['HOME'] + "/.ball_of_light/points.json"
+    end
+
+    def self.write_points(values)
+      Dir.mkdir(File.dirname points_file)
+      IO.write(points_file, values.to_json)
+    end
+
+    def self.additional_points
+      begin
+        points = JSON.parse(IO.read(points_file)) || []
+      rescue JSON::ParserError
+      rescue Errno::ENOENT
+      end
+      points ||= []
     end
   end
 end
