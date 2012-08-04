ff3b625297fbc2ccd6b49911126c2567aa6167c0
Marshall Yount <myount@zynga.com>
2012-08-04 17:47:38 -0500

    helper methods to find specific groups of lights

--- lib/ball_of_light/ball_of_light_controller.rb
+++ lib/ball_of_light/ball_of_light_controller.rb
@@ -32,5 +32,38 @@ module BallOfLight
       end
       points ||= []
     end
+
+    # yellow_ring = [5, 10, 11, 7, 4, 1]
+    # blue_ring   = [8, 12, 11, 6, 2, 1]
+    # red_ring    = [2, 5, 9, 12, 7, 3]
+    # green_ring  = [3, 4, 8, 9, 10, 6]
+
+    def yellow_ring_lights
+      [4, 9, 10, 6, 3, 0].map{|i| devices[i]}
+    end
+
+    def blue_ring_lights
+      [7, 11, 10, 5, 1, 0].map{|i| devices[i]}
+    end
+
+    def red_ring_lights
+      [1, 4, 8, 11, 6, 2].map{|i| devices[i]}
+    end
+
+    def green_ring_lights
+      [2, 3, 7, 8, 9, 5].map{|i| devices[i]}
+    end
+
+    def top_lights
+      [8,9,10,11].map{|i| devices[i]}
+    end
+
+    def bottom_lights
+      [0,1,2,3].map{|i| devices[i]}
+    end
+
+    def middle_lights
+      [4,5,6,7].map{|i| devices[i]}
+    end
   end
 end
