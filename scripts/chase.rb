db117b9df499cbfd9ed6b3072146622af6f9ca2a
Marshall Yount <myount@zynga.com>
2012-08-10 09:06:18 -0500

    revamp chase sequence

--- scripts/chase.rb
+++ scripts/chase.rb
@@ -19,18 +19,25 @@ controller = BallOfLight::BallOfLightController.new(options)
 
 
 controller.off!
-
+controller.bottom!
 # sequence = [5, 10, 11, 7, 4, 1, 8, 12, 11, 6, 2, 5, 9, 12, 7, 3, 6, 10, 9, 8, 4, 1]
 
+last = nil
 count = controller.chase_sequence.count
-[8.0, 6.0, 4.0, 2.0, 1.0, 1.0, 1.0, 1.0].each do |duration|
-  [0,1,2,3,8].map{|d| controller.devices[d]}.each do |light|
-#  controller.chase_sequence.each do |light|
+[32.0, 24.0, 16.0, 12.0, 8.0, 6.0, 4.0, 2.0, 2.0, 2.0, 2.0].each do |duration|
+  controller.chase_sequence.each do |light|
+    # change colors here
+    controller.buffer(:point => controller.random_color)
+    # fade in to each other
     controller.buffer(:dimmer => 0)
-    light.on
-    light.strobe_fast
-    controller.write!
-    sleep (duration/count)
+    controller.begin_animation!(:seconds => duration/count/2) do
+      last.dimmer(0) if last
+      light.on
+    end
+    # light.on
+    # controller.write!
+    sleep (duration/count/2)
+    last = light
   end
 end
 
@@ -38,5 +45,5 @@ controller.strobe_open
 controller.on!
 
 controller.animate!(:seconds => 2) do |animate|
-  animate.buffer(:dimmer => 0)
+  animate.buffer(:dimmer => 1)
 end
