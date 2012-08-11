e801927dedb82b1cd8763fd3762b8e41c778cbac
Marshall Yount <myount@zynga.com>
2012-08-11 17:44:20 -0500

    fix broken tests

--- lib/ball_of_light.rb
+++ lib/ball_of_light.rb
@@ -1,7 +1,7 @@
 require 'open_lighting'
 
 require_relative 'ball_of_light/ball_of_light_controller'
-require_relative 'ball_of_light/cli'
+require_relative 'ball_of_light/cli/cli'
 require_relative 'ball_of_light/joint'
 require_relative 'ball_of_light/user'
 require_relative 'ball_of_light/scene'
