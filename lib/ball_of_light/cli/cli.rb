5c468d86611a04fbe434bf3da07d2788582b0d3c
Marshall Yount <myount@zynga.com>
2012-08-12 09:15:34 -0500

    offer to kill pre-existing executables

--- lib/ball_of_light/cli/cli.rb
+++ lib/ball_of_light/cli/cli.rb
@@ -5,6 +5,16 @@ require 'io/console'
 require File.expand_path(File.dirname(__FILE__) + "/setup")
 require File.expand_path(File.dirname(__FILE__) + "/lights")
 require File.expand_path(File.dirname(__FILE__) + "/calibrate")
+["ola_streaming_client", "kinectable_pipe"].each do |exe|
+  running = `ps aux | grep #{exe} | grep -v grep`.strip
+  if running != ""
+    puts "You seem to have an instance of #{exe} running. Do you want me to kill it? (Y/n)"
+    unless STDIN.gets.downcase[/\An/]
+      pid = running.split(" ")[1]
+      `kill -9 #{pid}`
+    end
+  end
+end
 
 # Hey you: reader of this code!
 # This is a hacktastic mess.
