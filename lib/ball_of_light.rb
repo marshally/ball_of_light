08db8fec79d882de611be2fac098f781074781dc
Marshall Yount <myount@zynga.com>
2012-08-13 11:04:13 -0500

    add IoHelper class for non-blocking IO reads

--- lib/ball_of_light.rb
+++ lib/ball_of_light.rb
@@ -1,8 +1,8 @@
 require 'open_lighting'
 
 require_relative 'ball_of_light/ball_of_light_controller'
-require_relative 'ball_of_light/cli/cli'
 require_relative 'ball_of_light/joint'
 require_relative 'ball_of_light/user'
 require_relative 'ball_of_light/scene'
+require_relative 'ball_of_light/io_helper'
 require_relative 'ball_of_light/version'
---
+++ lib/ball_of_light/io_helper.rb
@@ -0,0 +1,26 @@
+class IoHelper
+  def self.gets_most_recent(io)
+    line = nil
+    read_ready, write_ready = IO.select([io], nil, nil, 0)
+    while read_ready && read = read_ready[0]
+      break if read.eof?
+      STDOUT.flush
+      line = read.gets
+      read_ready, write_ready = IO.select([io], nil, nil, 0)
+    end
+    line
+  end
+
+  def self.readall_nonblocking(io)
+    lines = []
+    read_ready, write_ready = IO.select([io], nil, nil, 0)
+    while read_ready && read = read_ready[0]
+      break if read.eof?
+      STDOUT.flush
+      lines << read.gets
+      read_ready, write_ready = IO.select([io], nil, nil, 0)
+    end
+    lines
+  end
+
+end
